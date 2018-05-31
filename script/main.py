c34736ca588062f3c9b0214dec8df0f4a6476fcf
Joonun Jang <joonun.jang@gmail.com>
2018-05-31 11:21:22 +0900

    [1] Default result directory was changed to result

--- script/main.py
+++ script/main.py
@@ -4,6 +4,8 @@ from yuweol_raw import Raw
 from dumper import dumper
 from analyzer import Analyzer
 
+RESULT_DIR="result"
+
 def test1(r1, r2):
 	size_x = 20
 	size_y = 20
@@ -17,22 +19,22 @@ def test1(r1, r2):
 	a1.set_region_label(size_x, size_y)
 	a2.set_region_label(size_x, size_y)
 	
-	d = dumper(".")
+	d = dumper(RESULT_DIR)
 	d.prepare_region(a1, size_x, size_y, "bot")
 	
-	td1 = dumper(".")
+	td1 = dumper(RESULT_DIR)
 	td1.prepare_region_distance(a1, size_x, size_y, "bot")
 	
-	td2 = dumper(".")
+	td2 = dumper(RESULT_DIR)
 	td2.prepare_region_direction(a1, size_x, size_y, "bot")
 	
-	td3 = dumper(".")
+	td3 = dumper(RESULT_DIR)
 	td3.prepare_region(a2, size_x, size_y, "human")
 	
-	td4 = dumper(".")
+	td4 = dumper(RESULT_DIR)
 	td4.prepare_region_distance(a2, size_x, size_y, "human")
 	
-	td5 = dumper(".")
+	td5 = dumper(RESULT_DIR)
 	td5.prepare_region_direction(a2, size_x, size_y, "human")
 	
 	d.merge(td1)
